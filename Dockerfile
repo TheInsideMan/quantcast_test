d6759107e6b830970ec1ede45fe9760768bcaf31
TheInsideMan <aaron.kenny@gmail.com>
2021-06-23 15:30:13 +0100

    d

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,14 @@
-# syntax = docker/dockerfile:1-experimental
-
 FROM --platform=${BUILDPLATFORM} golang:1.15.2-alpine AS base
 WORKDIR ./src/QuantCast
 ENV CGO_ENABLED=0
-COPY go.* .
+COPY . ./
 RUN --mount=type=cache,target=/go/pkg/mod \
     go mod download
 
 FROM base AS build
 ARG TARGETOS
 ARG TARGETARCH
-RUN --mount=target=. \
-    --mount=type=cache,target=/go/pkg/mod \
-    --mount=type=cache,target=/root/.cache/go-build \
-    GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -o /out/cookie_parser .
+RUN GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -o /out/cookie_parser .
 
 FROM base AS unit-test
 
--- main_test.go
+++ main_test.go
@@ -13,25 +13,13 @@ func TestSomething(t *testing.T) {
 	assert.Equal(t, 123, 123, "they should be equal")
 }
 
-
-// func TestEcho(t *testing.T) {
-// 	// Test happy path
-// 	err := echo([]string{"bin-name", "hello", "world!"})
-// 	require.NoError(t, err)
-// }
-
-// func TestEchoErrorNoArgs(t *testing.T) {
-// 	// Test empty arguments
-// 	err := echo([]string{})
-// 	require.Error(t, err)
-// }
-
-// func assertFileExists(t *testing.T) {
-// 	_, err := os.Lstat(path)
-// 	if err != nil {
-// 		if os.IsNotExist(err) {
-// 			assert.Fail(t, "unable to find file %q", path)
-// 		}
-// 		assert.Fail(t, "error when running os.Lstat(%q): %s", path, err)
-// 	}
-// }
+func assertFileExists(t *testing.T) {
+	path := "cookie_log.csv"
+	_, err := os.Lstat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			assert.Fail(t, "unable to find file %q", path)
+		}
+		assert.Fail(t, "error when running os.Lstat(%q): %s", path, err)
+	}
+}
