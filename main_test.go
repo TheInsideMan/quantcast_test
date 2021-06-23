b74883b9f559d35aa83b915d2b7f05f9971c86f1
TheInsideMan <aaron.kenny@gmail.com>
2021-06-23 15:56:12 +0100

    updating unit test

--- main_test.go
+++ main_test.go
@@ -8,19 +8,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomething(t *testing.T) {
-	fmt.Println("running TestSomething")
-  // assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
-}
-
-func assertFileExists(t *testing.T) {
-	path := "cookie_log2.csv"
-	_, err := os.Lstat(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			assert.Fail(t, "unable to find file %q", path)
-		}
-		assert.Fail(t, "error when running os.Lstat(%q): %s", path, err)
+func TestFileExists(t *testing.T) {
+	fmt.Println("running AssertFileExists")
+	path := "cookie_log.csv"
+	if _, err := os.Stat(path); err != nil {
+	  assert.Fail(t, fmt.Sprintf("unable to find file with path %v", path))
 	}
 }
